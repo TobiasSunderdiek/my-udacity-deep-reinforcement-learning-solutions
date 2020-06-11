81cf5fdbc68de3fba65367554b6282777b844cc5
TobiasSunderdiek <sunderdiek@gmail.com>
2020-06-11 18:15:10 +0000

    update q-table expected sarsa like

--- lab-taxi/README.md
+++ lab-taxi/README.md
@@ -1,1 +1,3 @@
 This exercise references to https://github.com/udacity/deep-reinforcement-learning/tree/master/lab-taxi
+
+Algo: Expected Sarsa
--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 class Agent:
 
-    def __init__(self, nA=6):
+    def __init__(self, nA=6, epsilon=0.1, alpha=0.05, gamma=1):
         """ Initialize agent.
 
         Params
@@ -12,6 +12,9 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +27,7 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        return self._epsilon_greedy_policy(state)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +40,21 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        self.Q[state][action] += self.Q[state][action] + self.alpha * (reward + self.gamma * self._expected_value_sum(next_state) - self.Q[state][action])
+
+    def _epsilon_greedy_policy(self, state):
+        return np.random.choice(self.nA)
+
+    def _expected_value_sum(self, next_state):
+        sum = 0
+        for action in range(self.nA):
+            sum += self._probability_of_action_in_next_step(next_state, action) * self.Q[next_state][action]
+        return sum
+
+    def _probability_of_action_in_next_step(self, next_state, action):
+        if (self.Q[next_state][action] == max(self.Q[next_state]):
+            probability = 1 - self.epsilon + (self.epsilon / self.nA)
+        else:
+            probability = self.epsilon / self.nA
+
+        return probability
